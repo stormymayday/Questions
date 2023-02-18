ed7d338bdf59de56bf9baf4901e9a6f62a1534e1
stormymayday <stormymayday7@gmail.com>
2023-02-17 21:03:00 -0700

    Implemented the funcionality using DOM traversal method. Fixed .question-text typo in CSS.

---
+++ app.js
@@ -0,0 +1,14 @@
+// traversing the dom
+const btns = document.querySelectorAll('.question-btn');
+
+btns.forEach(function (btn) {
+
+    btn.addEventListener('click', function (event) {
+
+        const question = event.currentTarget.parentElement.parentElement;
+
+        question.classList.toggle('show-text');
+
+    });
+
+});
