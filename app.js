36f00edde3510f9633d2955d6eb1d68d76868804
stormymayday <stormymayday7@gmail.com>
2023-02-18 23:41:42 -0700

    Implemnted functionality using selectors inside the element.

--- app.js
+++ app.js
@@ -1,11 +1,26 @@
 // traversing the dom
-const btns = document.querySelectorAll('.question-btn');
+// const btns = document.querySelectorAll('.question-btn');
 
-btns.forEach(function (btn) {
+// btns.forEach(function (btn) {
 
-    btn.addEventListener('click', function (event) {
+//     btn.addEventListener('click', function (event) {
 
-        const question = event.currentTarget.parentElement.parentElement;
+//         const question = event.currentTarget.parentElement.parentElement;
+
+//         question.classList.toggle('show-text');
+
+//     });
+
+// });
+
+// using selectors inside the element
+const questions = document.querySelectorAll('.question');
+
+questions.forEach(function (question) {
+
+    const btn = question.querySelector('.question-btn');
+
+    btn.addEventListener('click', function () {
 
         question.classList.toggle('show-text');
 
